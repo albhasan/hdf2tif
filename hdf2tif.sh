42646db5e1374f730b62d716d5eed8cc770e7091
albhasan <albhasan@gmail.com>
2017-11-26 20:23:41 -0200

    tested

--- hdf2tif.sh
+++ hdf2tif.sh
@@ -11,26 +11,12 @@ if [ $# -eq 1 ] ; then
     sds=$(      gdalinfo $hdf | grep "SUBDATASET_._NAME"  | awk -F "=" '{ print $2 }' | awk -F ":" '{print $1":"$2":"$3":"$4}')
     fnames=$(   gdalinfo $hdf | grep "SUBDATASET_._NAME"  | awk -F "=" '{ print $2 }' | awk -F ":" '{ print $5 }' |  tr -s ' ' | tr ' ' '_')
     tilenames=$(gdalinfo $hdf | grep "SUBDATASET_._NAME"  | awk -F "=" '{ print $2 }' | awk -F '"' '{ print $2 }' | awk -F "/" '{ print $NF }' | awk -F "." '{ print $1"_"$3"_"$4"_"$2 }')
-    array=($sds)                # subdata set names
-    array2=($fnames)            # output filenames
-    array3=($tilenames)         # info regarding the MODIS tile
-    for ((i=0;i<${#array[@]};++i)); do
-        bname=$(echo "${array2[i]}" | tr '_' ' ') # build band name from output filename
-        eval "gdal_translate -of GTiff '${array[i]}:$bname' ${array3[i]}_${array2[i]}.tif"
-    done
 elif [ $# -eq 2 ] ; then
-    echo "Export a single subdataset..."
+    #echo "Export a single subdataset..."
     sdsid=$2
     sds=$(      gdalinfo $hdf | grep "SUBDATASET_"$sdsid"_NAME"  | awk -F "=" '{ print $2 }' | awk -F ":" '{print $1":"$2":"$3":"$4}')
     fnames=$(   gdalinfo $hdf | grep "SUBDATASET_"$sdsid"_NAME"  | awk -F "=" '{ print $2 }' | awk -F ":" '{ print $5 }' |  tr -s ' ' | tr ' ' '_')
     tilenames=$(gdalinfo $hdf | grep "SUBDATASET_"$sdsid"_NAME"  | awk -F "=" '{ print $2 }' | awk -F '"' '{ print $2 }' | awk -F "/" '{ print $NF }' | awk -F "." '{ print $1"_"$3"_"$4"_"$2 }')
-    array=($sds)                # subdata set names
-    array2=($fnames)            # output filenames
-    array3=($tilenames)         # info regarding the MODIS tile
-    for ((i=0;i<${#array[@]};++i)); do
-        bname=$(echo "${array2[i]}" | tr '_' ' ') # build band name from output filename
-        eval "gdal_translate -of GTiff '${array[i]}:$bname' ${array3[i]}_${array2[i]}.tif"
-    done
 else
     echo 'ERROR: Wrong number of parameters'
     echo ''
@@ -40,6 +26,14 @@ else
     echo '  subdataset  Number. A subdataset ID (See gdalinfo)'
     exit 1
 fi
+
+array=($sds)                # subdata set names
+array2=($fnames)            # output filenames
+array3=($tilenames)         # info regarding the MODIS tile
+for ((i=0;i<${#array[@]};++i)); do
+    bname=$(echo "${array2[i]}" | tr '_' ' ') # build band name from output filename
+    eval "gdal_translate -of GTiff '${array[i]}:$bname' ${array3[i]}_${array2[i]}.tif"
+done
 
 exit 0
 
