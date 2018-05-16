1f18d646027e2a32655cab391487bb3fa6c3a722
albhasan <albhasan@gmail.com>
2018-05-16 17:04:28 -0300

    added dry-run option

--- hdf2tif_parallel.sh
+++ hdf2tif_parallel.sh
@@ -5,18 +5,21 @@
 # TODO:
 # ERROR 1: TIFFAppendToStrip:Maximum TIFF file size exceeded. Use BIGTIFF=YES creation option.
 #-------------------------------------------------------------------------------
-# ./hdf2tif_parallel.sh 12 10 0 0 400 400 /home/scidb/MOD13Q1 /home/scidb/alber/ghprojects/hdf2tif/tmp /home/scidb/alber/ghprojects/hdf2tif/output 16
+# ./hdf2tif_parallel.sh 12 10 0 0 4800 4800 512 512 /home/scidb/MOD13Q1 /home/scidb/alber/ghprojects/hdf2tif/tmp /home/scidb/alber/ghprojects/hdf2tif/output 16 0
 #-------------------------------------------------------------------------------
-#H=12
+#H=10
 #V=10
-FIRST=512
 #xoff=0
 #yoff=0
-#xsize=400
-#ysize=400
-#path_modis=/home/scidb/MODIS
-#path_output=/home/scidb/alber/ghprojects/hdf2tif/output
-#path_tmp="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+#xsize=4799
+#ysize=4799
+#first=10
+#nimgs=23
+#path_modis='/home/scidb/MODIS'
+#path_tmp='/home/scidb/Documents/tmp'
+#path_output='/home/scidb/Documents/tmp'
+#njobs=2
+#dry_run=3
 ################################################################################
 
 # Adapted from https://stackoverflow.com/questions/8455991/elegant-way-for-verbose-mode-in-scripts#33597663
@@ -49,10 +52,13 @@ if [ $# -ne 10 ] ; then
     echo '  yoff        Number. First pixel to export'
     echo '  xsize       Number of pixels to export'
     echo '  ysize       Number of pixels to export'
+    echo '  first       Index of the first image'
+    echo '  nimgs       Number of image to take starting at first'
     echo '  path_modis  Path to a repository of MODIS data (HDF files)'
     echo '  path_tmp    Path to a temporal directory'
     echo '  path_output Path to store the results'
-    echo '  njobs       Maximum number of jubs to run in parallel'
+    echo '  njobs       Maximum number of jobs to run in parallel'
+    echo '  dry_run     Any value different from 0 causes the script to print the list of files process and exit'
     exit 1
 fi
 
@@ -65,16 +71,30 @@ xoff=$3
 yoff=$4
 xsize=$5
 ysize=$6
-path_modis=$7
-path_tmp=$8
-path_output=$9
-njobs=$10
+first=$7
+nimgs=$8
+path_modis=$9
+path_tmp=$10
+path_output=$11
+njobs=$12
+dry_run=$13
 
 .log 6 "Getting files to export..."
-FILES=$(find -L "$path_modis" -type f | grep "MOD13Q1\.A[0-9]\{7\}\.h""$H""v""$V""\.006\.[0-9]\{13\}\.hdf$" | sort | head -n $FIRST)
+#sort_start=41
+#sort_end=47
+#FILES=$(find -L "$path_modis" -type f | grep "M[O|Y]D13Q1\.A[0-9]\{7\}\.h""$H""v""$V""\.006\.[0-9]\{13\}\.hdf$" | sort  -k 1.41,1.47 | head -n $(( $first + $nimgs )) | tail -n $nimgs)
+FILES=$(find -L "$path_modis" -type f | grep "MOD13Q1\.A[0-9]\{7\}\.h""$H""v""$V""\.006\.[0-9]\{13\}\.hdf$" | sort | head -n $(( $first + $nimgs )) | tail -n $nimgs)  
+FNAMES=($FILES)
+
+# Print the files and exit
+if [ $dry_run -ne 0 ] ; then
+    for ((i=0;i<${#FNAMES[@]};++i)); do
+        echo "${FNAMES[i]}"
+    done
+    exit 0
+fi
 
 .log 6 "Writting down the images's dates (julian)..." >> hdf2tif_parallel.log
-FNAMES=($FILES)
 for ((i=0;i<${#FNAMES[@]};++i)); do
     basename "${FNAMES[i]}" | awk -F "." '{ print $2 }' >> "$path_output"/MOD13Q1_h"$H"v"$V"_006_dates.txt
 done
