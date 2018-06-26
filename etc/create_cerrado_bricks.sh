556972bb0bc8b6c27ebdce3703b25657114acd56
albhasan <albhasan@gmail.com>
2018-06-26 15:10:10 -0300

    Fixed relative paths

--- etc/create_cerrado_bricks.sh
+++ etc/create_cerrado_bricks.sh
@@ -4,9 +4,9 @@
 #-------------------------------------------------------------------------------
 # Last update 2018-06-26
 ################################################################################
-.././hdf2tif_parallel.sh 12 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0 
-.././hdf2tif_parallel.sh 12 11 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
-.././hdf2tif_parallel.sh 13 09 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
-.././hdf2tif_parallel.sh 13 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
-.././hdf2tif_parallel.sh 13 11 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 12 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0 
+./hdf2tif_parallel.sh 12 11 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 13 09 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 13 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 13 11 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
 
