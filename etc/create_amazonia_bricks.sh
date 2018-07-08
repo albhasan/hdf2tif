d1aeeb6d7318d9a9bfcb2191e0dc57c79cf376d0
albhasan <albhasan@gmail.com>
2018-07-08 14:33:18 -0300

    added usage example

---
+++ etc/create_amazonia_bricks.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+################################################################################
+# CREATE THE MOD13Q1 TILES OF BRAZILIAN CERRADO
+#-------------------------------------------------------------------------------
+# Last update 2018-06-26
+################################################################################
+./hdf2tif_parallel.sh 10 08 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0 
+./hdf2tif_parallel.sh 10 09 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0 
+./hdf2tif_parallel.sh 11 08 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 11 09 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 11 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 12 08 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+./hdf2tif_parallel.sh 12 09 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+# cerrado
+#./hdf2tif_parallel.sh 12 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0 
+#./hdf2tif_parallel.sh 12 11 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+#./hdf2tif_parallel.sh 13 09 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+#./hdf2tif_parallel.sh 13 10 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+#./hdf2tif_parallel.sh 13 11 0 0 4800 4800 0 407 /home/scidb/MODIS/MOD13Q1 /home/scidb/tmp /disks/d7/hdf2tif_01_new 16 0
+
